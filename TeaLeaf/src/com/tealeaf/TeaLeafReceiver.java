e37f9e212e25b915a39b32bf10bff6d6ffde21c1
Vinod CG <vnodecg@gmail.com>
2014-07-10 19:18:07 +0530

    add reciever code

--- TeaLeaf/src/com/tealeaf/TeaLeafReceiver.java
+++ TeaLeaf/src/com/tealeaf/TeaLeafReceiver.java
@@ -9,25 +9,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.tealeaf.plugin.PluginManager;
+
 public class TeaLeafReceiver extends BroadcastReceiver {
 
-    @Override
-    public void onReceive(final Context context, final Intent intent) {
-        String action = intent.getAction();
-        
-        if (action.equals("com.android.vending.INSTALL_REFERRER")) {
-            
-            // log referrer
-            try {
-                Bundle bundle = intent.getExtras();
-                String referrer = URLDecoder.decode(bundle.getString("referrer"), "UTF-8");
+	@Override
+	public void onReceive(final Context context, final Intent intent) {
+		String action = intent.getAction();
+		if (action.equals("com.android.vending.INSTALL_REFERRER")) {
+			try {
+				PluginManager.callAll("setInstallReferrer", "");
+				Bundle bundle = intent.getExtras();
+				String referrer = URLDecoder.decode(bundle.getString("referrer"), "UTF-8");
 				Settings.build(context);
 				Settings settings = Settings.getInstance();
 				settings.setString("installReferrer.referrer", referrer);
 			} catch (UnsupportedEncodingException e) {
 				logger.log(e);
-			} 
+			}
 		}
-    }
+	}
 
 }
+
