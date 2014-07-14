3d891ff4b49091181da34fd9a382f7c1ae81f26a
Ramprasad Rajendran <r.ramprasad@gmail.com>
2014-07-14 22:16:04 +0530

    Change protocol for setInstallReferrer

--- TeaLeaf/src/com/tealeaf/plugin/IPlugin.java
+++ TeaLeaf/src/com/tealeaf/plugin/IPlugin.java
@@ -30,6 +30,6 @@ public interface IPlugin {
 	public void onDestroy();
 	public void onNewIntent(Intent intent);
 	public void onActivityResult(Integer request, Integer result, Intent data);
-	public void setInstallReferrer(String referrer);
+	public void setInstallReferrer(Context context, Intent intent);
 
 }
